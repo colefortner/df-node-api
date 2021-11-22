02260299e6ec67deb6dc0ed0173ae79b696b4fbb
Cole Fortner <colefortner@gmail.com>
2021-11-22 16:28:32 -0500

    Change getUsers so we can grab all users from our dtabase via the / route

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -13,9 +13,16 @@ const users = [
   }
 ];
 
-const getUsers = (req, res, next) => {
+const getUsers = async (req, res, next) => {
   // sends back all users
-  res.json({ users: users });
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError("Fetching users failed", 500);
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
