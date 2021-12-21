5c538597d600f20233009cc697bb485afdeb2ba9
Cole Fortner <colefortner@gmail.com>
2021-12-21 13:28:00 -0500

    Change some status codes to be a bit more explicit

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -98,7 +98,7 @@ const login = async (req, res, next) => {
   }
 
   if (!existingUser) {
-    const error = new HttpError("Invalid credentials", 401);
+    const error = new HttpError("Invalid credentials", 403);
     return next(error);
   }
 
@@ -116,7 +116,7 @@ const login = async (req, res, next) => {
   if (!isValidPassword) {
     const error = new HttpError(
       "Invalid credentials, could not log you in",
-      401
+      403
     );
     return next(error);
   }
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
-    const error = new HttpError("Authenticatoin failed!", 401);
+    const error = new HttpError("Authenticatoin failed!", 403);
     return next(error);
   }
 };
